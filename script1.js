3bdc87de6ea3259b85dfc8a152aed15a67225989
JosephEnr <56930038+JosephEnr@users.noreply.github.com>
2020-02-13 04:48:30 -0800

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,13 @@
+function start(){
+  alert("You da best");
+}
+function fav(){
+  var favs = Joseph 
+  var ques = readLine("whos your favorite student?");
+  if(ques==favs){
+    document.getElementById("answer").innerHTML="correct!!!";
+  }
+  else{
+    document.getElementById("answer").innerHTML="woops wrong anser";
+  }
+}
